58ce1ad8c44cecda88583d37e5cc3db471aa6ef2
Matheus Pereira dos Santos <pereiramatheus064@gmail.com>
2022-02-27 01:18:12 -0300

    update

--- microservices-node/server.js
+++ microservices-node/server.js
@@ -3,7 +3,7 @@ const weatherApp = express();
 
 const port = 5000;
 
-const routes = require("./api/routes");
+const routes = require("./api_source/routes");
 routes(weatherApp);
 
 weatherApp.listen(port, () => {
