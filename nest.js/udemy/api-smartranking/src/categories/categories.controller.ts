d1d4998931b20c75a76b504f497062c1be7e12c4
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2022-05-19 12:12:46 -0300

    28. Entidade categories

--- nest.js/udemy/api-smartranking/src/categories/categories.controller.ts
+++ nest.js/udemy/api-smartranking/src/categories/categories.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dtos/create-category.dto';
 import { Category } from './interface/category.interface';
@@ -11,5 +11,15 @@ export class CategoriesController {
     @UsePipes(ValidationPipe)
     async createCategory(@Body() createCategoryDto: CreateCategoryDto): Promise<Category> {
         return await this.categoriesService.createCategory(createCategoryDto);
+    }
+
+    @Get()
+    async consultCategories(): Promise<Array<Category>> {
+        return await this.categoriesService.consultAllCategories();
+    }
+
+    @Get("/:category")
+    async getCategoryById(@Param("category") category: string): Promise<Category> {
+        return await this.categoriesService.getCategory(category);
     }
 }
--- nest.js/udemy/api-smartranking/src/categories/categories.service.ts
+++ nest.js/udemy/api-smartranking/src/categories/categories.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateCategoryDto } from './dtos/create-category.dto';
@@ -19,5 +19,13 @@ export class CategoriesService {
         return await createdCategory.save();
     }
 
+    async consultAllCategories(): Promise<Array<Category>> {
+        return await this.categoryModel.find().exec();
+    }
 
+    async getCategory(category): Promise<Category> {
+        const foundCategory = await this.categoryModel.findOne({ category }).exec();
+        if (!foundCategory) throw new NotFoundException(`The category ${category} already not exist.`);
+        return foundCategory;
+    }
 }
