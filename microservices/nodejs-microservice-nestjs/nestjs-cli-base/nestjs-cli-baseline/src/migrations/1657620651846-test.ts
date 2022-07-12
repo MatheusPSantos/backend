c7c656bf3458e03f77de60f9711447c7dfdfb2ec
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2022-07-12 07:19:34 -0300

    basic configs

---
+++ microservices/nodejs-microservice-nestjs/nestjs-cli-base/nestjs-cli-baseline/src/migrations/1657620651846-test.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class test1657620651846 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
