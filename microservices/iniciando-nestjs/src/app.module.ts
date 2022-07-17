5774e028c6587232582549cb70330619374390cb
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2022-07-16 23:26:28 -0300

    create repository

--- microservices/iniciando-nestjs/src/app.module.ts
+++ microservices/iniciando-nestjs/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoryController } from './category/category.controller';
+import { Category } from './category/category.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      // @ts-ignore
+      type: process.env.TYPEORM_CONNECTION,
+      host: process.env.TYPEORM_HOST,
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      port: parseInt(process.env.TYPEORM_PORT),
+      entities: [Category]
+    }),
+    TypeOrmModule.forFeature([Category])
+  ],
   controllers: [AppController, CategoryController],
   providers: [AppService],
 })
--- microservices/iniciando-nestjs/src/category/category.controller.ts
+++ microservices/iniciando-nestjs/src/category/category.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Category } from './category.entity';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+
+    constructor(
+        @InjectRepository(Category)
+        private categoryRespository: Repository<Category>
+    ) { }
+
+    @Get()
+    async index() {
+        return await this.categoryRespository.find();
+    }
+
+    @Get('category-create')
+    async categoryCreate() {
+        const category = await this.categoryRespository.create({ name: 'category test' });
+        return await this.categoryRespository.save(category);
+    }
+}
