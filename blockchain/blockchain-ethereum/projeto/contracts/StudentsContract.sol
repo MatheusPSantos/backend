41f7bc8a508ec7afb6342b3c4c87b3da4748c58f
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2022-07-21 22:30:07 -0300

    compile and test

--- blockchain/blockchain-ethereum/projeto/contracts/StudentsContract.sol
+++ blockchain/blockchain-ethereum/projeto/contracts/StudentsContract.sol
@@ -2,18 +2,14 @@
 pragma solidity =0.8.12;
 
 contract StudentsContract {
-
-
     struct Student {
         string studentName;
         uint256 studentAge;
     }
 
-    
     mapping(address => Student) private _enrolledStudents;
 
-
-    function getEnrolledStudentsByAddress(address _studentAddress)
+    function getEnrolledStudentByAddress(address _studentAddress)
         public
         view
         returns (Student memory)
@@ -21,15 +17,13 @@ contract StudentsContract {
         return _enrolledStudents[_studentAddress];
     }
 
-
-    function isStudentAgeValid(uint _studentAge)
+    function isStudentAgeValid(uint256 _studentAge)
         private
         pure
         returns (bool)
     {
         return _studentAge > 0;
     }
-
 
     function isStudentNameValid(string memory _studentName)
         private
@@ -40,40 +34,25 @@ contract StudentsContract {
         return tempString.length > 0;
     }
 
-
     function isStudentValid(Student memory _student)
         private
         pure
         returns (bool)
     {
-        return isStudentNameValid(_student.studentName) &&
+        return
+            isStudentNameValid(_student.studentName) &&
             isStudentAgeValid(_student.studentAge);
     }
 
-
-    function enrollStudent(
-        string memory _studentName,
-        uint _studentAge
-    )
-    public
+    function enrollStudent(string memory _studentName, uint256 _studentAge)
+        public
     {
+        require(isStudentNameValid(_studentName), "Name must be informed");
+        require(isStudentAgeValid(_studentAge), "Age must be informed");
         require(
-            isStudentNameValid(_studentName),
-            "Name must be informed"
-        );
-        
-        require(
-            isStudentAgeValid(_studentAge),
-            "Age must be informed"
-        );
-
-        require(
-            !isStudentValid(
-                getEnrolledStudentsByAddress(msg.sender)
-            ),
+            !isStudentValid(getEnrolledStudentByAddress(msg.sender)),
             "Student already enrolled"
         );
-
 
         Student memory student;
         student.studentName = _studentName;
@@ -81,8 +60,6 @@ contract StudentsContract {
 
         assert(isStudentValid(student));
 
-        _enrolledStudents[msg.sender] = student;        
+        _enrolledStudents[msg.sender] = student;
     }
-
-
 }
